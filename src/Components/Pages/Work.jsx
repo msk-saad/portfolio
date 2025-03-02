83879ae729df1d32ae30fc009bab92a27a830213
msk-saad <developersaad12@gmail.com>
2025-03-02 21:17:41 +0530

    added experience page

--- src/Components/Pages/Work.jsx
+++ src/Components/Pages/Work.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../../styles/work.css'
+import workData from '../Data/workData.json'
 
 const Work = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(workData);
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Work Page</h1>
-      <p>This is the work content.</p>
+    <>
+      <div className="work-container">
+        <div className="header">
+          <h1>{data.header.title}</h1>
+          <h3>{data.header.description}</h3>
+        </div>
+
+        {data.experience.map((experience, index) => (
+          <div key={index} className="experience-container">
+            <div className="workcity">
+              <div className="city">
+                {experience.city}
+              </div>
+
+              <div className="timelime">
+                {experience.timeline}
+              </div>
+            </div>
+
+            <div className="workplace">
+              <h1>{experience.position} <br /> </h1>
+              <p>@{experience.company}</p>
+
+              <hr className='text-zinc-600 opacity-65' />
+            </div>
+
+            <div className="work-desc">
+              {experience.description.map((desc, idx) => (
+                <p key={idx}>{desc}</p>
+              ))}
+            </div>
+
+            <div className="work-skills">
+              {experience.skills.map((skill, idx) => (
+                <div key={idx} className='skill'>
+                  <p>{skill}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
     </div>
+    </>
   );
 };
 
